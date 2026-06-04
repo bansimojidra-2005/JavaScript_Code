385a8e73ffe055fc0404e131766eefd1de1b994f
Bansi <bansimojidra@gmail.com>
2026-06-04 10:43:19 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newr version
+
+// alert(3 + 3) // We are using nodejs, not browser
+
+console.log(3 
+    + 
+    3);  // Code redability should be high
+
+console.log("Bansi");
+
+
+let name = "bansi"
+let age = 18
+let isLoggenIn = false
+
+
+// number => 2 to pwer 53
+// bigint
+// string => "", ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
