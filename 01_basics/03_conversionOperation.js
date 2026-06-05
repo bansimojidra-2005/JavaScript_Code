df448d05db0e7661e3bfb4a3e769ca3facc4bc34
Bansi <bansimojidra@gmail.com>
2026-06-05 10:09:50 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => e
+// undefined => NaN
+
+// let isLoggedIn = 0
+let isLoggedIn = ""
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
