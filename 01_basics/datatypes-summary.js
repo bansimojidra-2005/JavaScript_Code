e413c73074cabfde39831a1c5fa13f7caf2b147b
Bansi <bansimojidra@gmail.com>
2026-06-09 09:26:16 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -36,3 +36,30 @@ console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myCodename = "javaScriptCode"
+
+let anothername = myCodename
+anothername = "scriptcode"
+
+console.log(myCodename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@byl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
